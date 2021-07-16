7ce68c2995b1be9c2d0202f8079ca3ac20e84446
JChan91 <fourbin1221@gmail.com>
2021-07-16 23:10:55 +0900

    Init Spring Boot Project

---
+++ src/main/java/com/team/snackpedia/SnackPediaApplication.java
@@ -0,0 +1,13 @@
+package com.team.snackpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackPediaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SnackPediaApplication.class, args);
+  }
+
+}
